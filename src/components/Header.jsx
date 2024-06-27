69cdbb898be2e8e76fcd517594ae5912fb0b82bb
saptarsidebnath98 <saptarsidebnath666666@gmail.com>
2024-06-27 08:12:42 +0530

    made a custom componenet named CartContextProvider

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
 import { useRef } from 'react';
+import { useContext } from 'react';
 
 import CartModal from './CartModal.jsx';
+import { CartContext } from '../store/shopping-cart-context.jsx';
 
-export default function Header({ cart, onUpdateCartItemQuantity }) {
+export default function Header() {
   const modal = useRef();
+  const { items, addItemToCart } = useContext(CartContext);
 
-  const cartQuantity = cart.items.length;
+  const cartQuantity = items.length;
 
   function handleOpenCartClick() {
     modal.current.open();
@@ -24,10 +27,7 @@ export default function Header({ cart, onUpdateCartItemQuantity }) {
 
   return (
     <>
-      <CartModal
-        ref={modal}
-        cartItems={cart.items}
-        onUpdateCartItemQuantity={onUpdateCartItemQuantity}
+      <CartModal ref={modal}
         title="Your Cart"
         actions={modalActions}
       />
